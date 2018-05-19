5b83a71802d1d8d5aa8f3d2d1a5f567bc7e0dc8b
Piotr Domowicz <p.domowicz@p200300cc33cbd8011494fb0b24f5828c.dip0.t-ipconnect.de>
2018-05-19 07:06:55 +0200

    - Added GPL statement for each file

--- ReusableScrollView/ReusableScrollView.h
+++ ReusableScrollView/ReusableScrollView.h
@@ -1,10 +1,25 @@
-//
 //  ReusableScrollView.h
-//  ReusableScrollView
-//
-//  Created by sumofighter666 on 06.05.18.
-//  Copyright © 2018 sumofighter666. All rights reserved.
-//
+
+/*
+ 
+ Copyright (c) 2018 ReusableScrollView (https://github.com/sumofighter666/ReusableScrollView)
+ 
+ This file is part of ReusableScrollView.
+ 
+ Foobar is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Foobar is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
 
 #import <UIKit/UIKit.h>
 
